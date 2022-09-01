5f2863f4161a2898146d2551119933b796dfb776
SWAPNIL SRIVASTAVA <swapnil200224@gmail.com>
2022-09-02 02:11:01 +0530

    State 9-11

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import React,{ useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode,setMode] = useState("light");
+  
+  const ToggleMode = ()=> {
+    if(mode === "light")
+    {
+        setMode("dark");
+        document.body.style.backgroundColor = "#031a2e";
+        
+    }
+    else
+    {
+        setMode("light");
+        document.body.style.backgroundColor = "white";
+    } 
+  }
+
   return (
     <>
-    <Navbar title = "TextUtils" About = "About Us"/>
+    <Navbar mode = {mode} toggleMode = {ToggleMode} title = "TextUtils" About = "About Us"/>
     {/* <Navbar title = {3} About = "About Us"/> */}
     {/* <Navbar title = "TextUtils"/> */}
 
-    <TextForm heading = "Enter Text to Analyze"/>
+    <TextForm heading = "Enter Text to Analyze" mode = {mode}/>
+
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,52 +2,53 @@ import React,{useState} from 'react'
 
 
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter Text Here");
+    const [text, setText] = useState("");
 
     const UpHandle = (e)=> {
         e.preventDefault();  //To Stop page from Reloading
-        console.log("Uppercase Was Clicked");
+        // console.log("Uppercase Was Clicked");
         let NewText = text.toUpperCase();
         setText(NewText);
     }
 
     const Onnchange = (event)=> {
-        console.log("Typing...");
+        // console.log("Typing...");
         setText(event.target.value)
     }
 
     const LoHandle = (e)=> {
         e.preventDefault();  //To Stop page from Reloading
-        console.log("Lowercase Was Clicked");
+        // console.log("Lowercase Was Clicked");
         let NewText = text.toLowerCase();
         setText(NewText);
     }
 
+    const ClearHandle = (e)=> {
+        e.preventDefault();  //To Stop page from Reloading
+        let Newtext = "";
+        setText(Newtext);
+    }
 
     return (
     <>
-    <form className="container">
+    <form className="container my-3" style = {{color: props.mode === "light" ? "black" : "white"}}>
   
         <div className="mb-3 my-3">
             <h3>{props.heading}</h3>
-            <textarea className="form-control" value={text} id="myBox" rows = "8" onChange={Onnchange}></textarea>
+            <textarea className="form-control" value={text} id="myBox" rows = "8" onChange={Onnchange} style = {{backgroundColor: props.mode === "light" ? "white": "grey" , color:props.mode === "light" ? "black" : "white"}} ></textarea>
         </div>
         
-        <button className="btn btn-primary" onClick={UpHandle}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-3" onClick={LoHandle} >Convert to Lowercase</button>
-        
-        
+        <button className="btn btn-primary mx-1" onClick={UpHandle}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={LoHandle} >Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={ClearHandle} >Clear Text</button>       
     </form>
 
-    <div className="container my-3">
+    <div className="container my-3" style = {{color: props.mode === "light" ? "black" : "white"}}>
         <h3>Your Text Summary</h3>
         <p>{text.split(" ").length} words {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} minutes read</p>
-    </div>
-
-    <div className="container">
+        <p>{0.008 * text.split(" ").length} minutes read</p>    
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter Something To Preview"}</p>
     </div>
   
     </>
