94c25dd4099ecc613ca8aa70c59027df59301b2a
SWAPNIL SRIVASTAVA <75637474+STRYKER2403@users.noreply.github.com>
2022-09-06 03:13:13 +0530

    Updates

---
+++ precache-manifest.6ce7a644599616a6d4f4eaa5da6c3951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6b0fb02d30d7c930a8f",
+    "url": "/Textutils_react/static/css/main.88e501bd.chunk.css"
+  },
+  {
+    "revision": "e6b0fb02d30d7c930a8f",
+    "url": "/Textutils_react/static/js/main.e6b0fb02.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Textutils_react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Textutils_react/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "00beafdb54f3e9d4fd06",
+    "url": "/Textutils_react/static/js/runtime~main.00beafdb.js"
+  },
+  {
+    "revision": "f2f2e9fcb8d5ad4c7b73646a347460f3",
+    "url": "/Textutils_react/index.html"
+  }
+];
