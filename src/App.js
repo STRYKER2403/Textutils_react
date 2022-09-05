0981624a9f137d8a611862bd66dc747ff69386d8
SWAPNIL SRIVASTAVA <75637474+STRYKER2403@users.noreply.github.com>
2022-09-06 02:40:26 +0530

    state 15-16

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React,{ useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm';
+
+    import {
+      BrowserRouter as Router,
+      Routes,
+      Route
+    } from "react-router-dom";
+
 
 function App() {
   const [mode,setMode] = useState("light");
@@ -25,22 +33,44 @@ function App() {
         setMode("dark");
         document.body.style.backgroundColor = "#031a2e";  
         showAlert("Dark Mode Has Been Enabled","success");
+        document.title = "TextUtils - Dark mode";
+
+        // setInterval(()=>{
+        //   document.title = "TextUtils is Amazing";
+        // },2000);
+
+        // setInterval(()=>{
+        //   document.title = "TextUtils is Amazingly Good";
+        // },1500);
     }
     else
     {
         setMode("light");
         document.body.style.backgroundColor = "white";
         showAlert("Light Mode Has Been Enabled","success");
+        document.title = "TextUtils - Light mode";
     } 
   }
 
   return (
     <>
+    <Router>
     <Navbar  mode = {mode} toggleMode = {ToggleMode} title = "TextUtils" About = "About Us"/>
+    <Alert alert = {alert}/>
+
+    <Routes>
+          <Route exact path="/about" element = {<About />} />
+          <Route exact path="/" element = {<TextForm heading = "Enter Text to Analyze" mode = {mode} showAlert = {showAlert}/>} />         
+    </Routes>
+    </Router>
+    
+
+    {/* <Navbar  mode = {mode} toggleMode = {ToggleMode} title = "TextUtils" About = "About Us"/> */}
     {/* <Navbar title = {3} About = "About Us"/> */}
     {/* <Navbar title = "TextUtils"/> */}
-    <Alert alert = {alert}/>
+    {/* <Alert alert = {alert}/>
     <TextForm heading = "Enter Text to Analyze" mode = {mode} showAlert = {showAlert}/>
+    <About></About> */}
     </>
   );
 }
